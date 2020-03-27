a5f58666e5aadebd12a24d880ec2f980b11dc1ee
joaolsa <joaoluizdoss@gmail.com>
2020-03-27 17:27:53 -0300

    remove db.sqlite

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
-const app = express();
+const app = express()
+const PORT = 3333
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.listen(3333);
-
+app.listen(PORT)
+console.log(`Server running at localhost:${PORT}`)
